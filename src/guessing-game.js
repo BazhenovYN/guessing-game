6d84bde6ac1aef40912527e7bbaab7d647d0ae2f
BazhenovYN <BazhenovYN@gmail.com>
2023-02-26 21:51:26 +0300

    feat: done guessing-game

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
-    constructor() {}
-
+    constructor() {
+        this._left = 0;
+        this._right = 0;
+        this._middle = 0;
+    }
+    
     setRange(min, max) {
-
+        this._left = min;
+        this._right = max;
     }
 
     guess() {
-
+        this._middle = Math.ceil((this._left + this._right) / 2);
+        return this._middle;
     }
 
     lower() {
-
+        this._right = this._middle;
     }
 
     greater() {
-
+        this._left = this._middle;
     }
 }
 
